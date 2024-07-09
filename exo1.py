fe4e5b206ae28618d17964fe03f8f424bfbe585f
Alfredveve <128948530+Alfredveve@users.noreply.github.com>
2024-07-09 04:16:27 +0000

    Ajout de fonction somme

---
+++ exo1.py
@@ -0,0 +1,13 @@
+def multiplication(a, b):
+    return a * b
+
+
+print('le produit est ', multiplication(2, 3))
+
+# Ajout de fonction somme après la création de la branche testbr1
+
+def somme(x, y):
+    return x + y
+
+
+print("la somme de x et y est : ", somme(2,3))
